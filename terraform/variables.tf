523676529a1ae2e8b0349a7af8201b6be56caea3
Your Name <ironmanfbstark@gmail.com>
2026-04-02 22:33:14 +0500

    refactor: clean repo structure, remove duplicates, improve gitignore

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,24 @@
-variable "aws_region"      { default = "us-east-1" }
-variable "cluster_name"    { default = "dev-eks-cluster" }
-variable "vpc_name"        { default = "dev-vpc" }
-variable "server_cert_arn" { type = string }
-variable "client_cert_arn" { type = string }
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "dev-eks-cluster"
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  default     = "dev-vpc"
+}
+
+variable "server_cert_arn" {
+  description = "ACM ARN for VPN server certificate"
+  type        = string
+}
+
+variable "client_cert_arn" {
+  description = "ACM ARN for VPN client certificate"
+  type        = string
+}
