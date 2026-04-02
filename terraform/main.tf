8a7fcfeef98cd2ef3fd6e3d765bc4083d906358c
Your Name <ironmanfbstark@gmail.com>
2026-04-03 02:31:46 +0500

    trigger: redeploy with new web layer routes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    }
-  }
-}
+provider "helm" {}
 
 data "aws_caller_identity" "current" {}
 
